17e4241a849c7f936bec2b04e440d64350e82360
zerazkm <osasuwu22@gmail.com>
2026-09-04 23:39:25 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/week01/CircleCalculator.kt
@@ -0,0 +1,14 @@
+package oop_133907_SebastianReinhart.week01
+
+fun main() {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
+    println(checkSize(area))
+}
+
+fun checkSize(area: Double) =
+    if (area > 100) "This is a Big Circle"
+    else "This is a Small Circle"
