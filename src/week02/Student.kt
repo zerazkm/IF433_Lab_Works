5aea14e14ad84e62778ab217fa22a21b1c406ae2
zerazkm <osasuwu22@gmail.com>
2026-09-11 20:13:45 +0700

    week02: add warning validation in init block

--- src/week02/Student.kt
+++ src/week02/Student.kt
@@ -6,4 +6,12 @@ class Student(
     var major: String
 ) {
 
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM $nim yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
