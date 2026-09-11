09337f988f80dabf704c8bbb7b4b621832783881
zerazkm <osasuwu22@gmail.com>
2026-09-11 20:16:21 +0700

    week02: challenge refactor primary constructor using default args

--- src/week02/Student.kt
+++ src/week02/Student.kt
@@ -1,9 +1,12 @@
 package oop_133907_SebastianReinhart.week02
 
+package oop_<nim>_<nama>.week02
+
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
 
     init {
