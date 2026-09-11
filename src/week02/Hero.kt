9d68bc74338c911ad220a6229702ab10c5f35cba
zerazkm <osasuwu22@gmail.com>
2026-09-11 20:18:01 +0700

    week02: (task 2) create Hero class

--- src/week02/Hero.kt
+++ src/week02/Hero.kt
@@ -1,2 +1,24 @@
 package oop_133907_SebastianReinhart.week02
 
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
