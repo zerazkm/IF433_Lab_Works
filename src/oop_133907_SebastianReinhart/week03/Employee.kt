25de1ba95bdbf26d5d54d604ac676325793d3879
Sebastian Reinhart <osasuwu22@gmail.com>
2026-09-19 10:30:40 +0700

    week04: create basic Vehicle parent class

---
+++ src/oop_133907_SebastianReinhart/week03/Employee.kt
@@ -0,0 +1,27 @@
+    package oop_133907_SebastianReinhart.week03
+
+    class Employee(val name: String) {
+
+        var salary: Int = 0
+            set(value) {
+                if (value < 0) {
+                    println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                    field = 0
+                } else {
+                    field = value
+                }
+            }
+
+        private var performanceRating: Int = 3
+
+        fun increasePerformance() {
+            performanceRating++
+        }
+
+        fun printStatus() {
+            println("Karyawan: $name, Rating: $performanceRating")
+        }
+
+        val annualSalary: Int
+            get() = salary * 12
+    }
---
+++ src/oop_133907_SebastianReinhart/week04/Vehicle.kt
@@ -0,0 +1,14 @@
+package oop_133907_SebastianReinhart.week04
+
+open class Vehicle(val brand: String) {
+    var speed: Int = 0
+
+    open fun accelerate() {
+        speed += 10
+        println("$brand melaju. Kecepatan: $speed km/jam")
+    }
+
+    open fun honk() {
+        println("Beep beep!")
+    }
+}
